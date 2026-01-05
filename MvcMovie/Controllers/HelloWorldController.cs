5809bed2b0d70045dcd7003f0b0190252fa7d834
Now <khj990322@gmail.com>
2026-01-06 01:07:54 +0900

    feat: 컨트롤러 추가

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers; // 이름 막 적어도 동작은 하지만, 다른곳에서 사용하기 위해 명확한 네임스페이스 지정하는게 좋음
+
+public class HelloWorldController : Controller
+{
+    //GET: /HelloWorld/
+    public string Index()
+    {
+        return "This is my default action...";
+    }
+
+    //GET: /HelloWorld/Welcome/
+    public string Welcome()
+    {
+        return "This is the Welcome action method...";
+    }
+}
