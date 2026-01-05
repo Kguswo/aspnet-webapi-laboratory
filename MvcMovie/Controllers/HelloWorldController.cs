a07979324dc500e8d5861de274822560df69515b
Now <khj990322@gmail.com>
2026-01-06 02:08:24 +0900

    feat: 문자열 보간 및 모델 바인딩 사용 메서드 추가

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -1,18 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
 
 namespace MvcMovie.Controllers; // 이름 막 적어도 동작은 하지만, 다른곳에서 사용하기 위해 명확한 네임스페이스 지정하는게 좋음
 
 public class HelloWorldController : Controller
 {
-    //GET: /HelloWorld/
+    // GET: /HelloWorld/
     public string Index()
     {
         return "This is my default action...";
     }
 
-    //GET: /HelloWorld/Welcome/
+    ////GET: /HelloWorld/Welcome/
     public string Welcome()
     {
         return "This is the Welcome action method...";
+    }
+
+    // GET: /HelloWorld/Welcome/
+    // Requires using System.Text.Encodings.Web;
+    public string Welcome(string name, int numTimes = 1)
+    {
+        return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}"); //  HtmlEncoder.Default.Encode로 악의적인 입력으로부터 앱을 보호함
+    }
+
+    public string Welcome2(string name, int ID = 1)
+    {
+        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
     }
 }
