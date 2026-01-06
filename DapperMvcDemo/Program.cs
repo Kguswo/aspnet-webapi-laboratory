ab7fcf1958b6b15bfa6616f3e1f4cd7088d2fcdd
Now <khj990322@gmail.com>
2026-01-07 03:06:54 +0900

    chore: DapperMvcDemo 프로젝트 생성
    Dapper을 통한 .NET Core MVC와 MSSQL 통신 실습

---
+++ DapperMvcDemo/Program.cs
@@ -0,0 +1,33 @@
+using DapperMvcDemo.Data;
+using DapperMvcDemo.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddSingleton<DapperDbContext>(); // DapperDbContext를 싱글톤으로 등록
+builder.Services.AddScoped<IProductRepository, ProductRepository>(); // IProductRepository 구현체 등록
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
