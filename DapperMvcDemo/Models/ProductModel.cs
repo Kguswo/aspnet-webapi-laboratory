2c0ba9b3df8f8abb184b58736c0de20f75c47ab6
Now <khj990322@gmail.com>
2026-01-07 03:08:09 +0900

    feat: ProductModel 클래스 추가 및 한글 Display 적용

---
+++ DapperMvcDemo/Models/ProductModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DapperMvcDemo.Models
+{
+    public class ProductModel
+    {
+        public Guid ProductId { get; set; }
+
+        [Display(Name = "상품명")]
+        public string ProductName { get; set; }
+
+        [Display(Name = "가격")]
+        public decimal Price { get; set; }
+
+        [Display(Name = "상품 설명")]
+        public string ProductDescription { get; set; }
+
+        [Display(Name = "등록일")]
+        public DateTime? CreatedOn { get; set; }
+
+        [Display(Name = "수정일")]
+        public DateTime? UpdatedOn { get; set; }
+    }
+}
