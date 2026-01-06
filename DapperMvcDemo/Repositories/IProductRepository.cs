9b5482d49ff2db360f043c2053081480e6e7443e
Now <khj990322@gmail.com>
2026-01-07 03:11:46 +0900

    feat: IProductRepository 인터페이스 추가
    ProductModel에 대한 비동기 CRUD 메서드를 정의한 IProductRepository 인터페이스를 DapperMvcDemo.Repositories 네임스페이스에 새로 추가하였습니다. 각 메서드는 Task<T>를 반환하여 비동기 처리를 지원합니다.

---
+++ DapperMvcDemo/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using DapperMvcDemo.Models;
+
+namespace DapperMvcDemo.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductModel>> GetAll(); // Task<T>란 비동기 작업의 결과를 나타내는 제네릭 인터페이스입니다. (Java의 CompletableFuture<T>) 
+        Task<ProductModel> GetById(Guid id); // global unique identifier
+        Task<ProductModel> Create(ProductModel model);
+        Task<ProductModel> Update(ProductModel model);
+        Task Delete(Guid id);
+    }
+}
